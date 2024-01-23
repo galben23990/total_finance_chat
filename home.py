baa780ad4367f85c86399454729519ac199c94bf
galben23990 <153602946+galben23990@users.noreply.github.com>
2024-01-23 23:50:58 +0200

    Update home.py

--- home.py
+++ home.py
@@ -29,11 +29,11 @@ x="sk-9xPQ9C50b"
 y="c1sYkg2yikQT3Bl"
 z="bkFJ6jlVHQrpiJT3KZ9BmOMP"
 openai.api_key = x+y+z
+openaiclient = openai.OpenAI(api_key=openai.api_key )
 
 
 def ask_gpt(massage_history,model="gpt-4-1106-preview",max_tokens=2000,temperature=0,return_str=True,response_format={"type": "json_object"}):
    
-    openaiclient = openai.OpenAI(api_key=openai.api_key )
 
     response =  openaiclient.chat.completions.create(
       model=model,
